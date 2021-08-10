0b2d46e31fc3435bb45386bfd447b58f694ae96c
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-10 14:44:29 -0300

    Adicione exercício 4.3

---
+++ lista4/3aleatorio.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+void verifiqueContinuacao(int palpite) {
+  if (palpite < 0) {
+    cout << "Você perdeu!" << endl;
+    exit(0);
+  }
+}
+
+void classifiquePalpite(int aleatorio, int palpite) {
+  if (palpite > aleatorio) {
+    cout << "Número muito grande!";
+  }
+  if (palpite < aleatorio) {
+    cout << "Número muito pequeno!";
+  }
+}
+
+int main() {
+  int palpite, tentativas;
+  cout << "Digite um número negativo se quiser parar.";
+
+  srand(time(NULL));
+  const int aleatorio = rand() % 100 + 1;
+
+  for (tentativas = 0; palpite != aleatorio; ++tentativas) {
+    cout << " Tente adivinhar o número (de 0 a 99): ";
+    cin >> palpite;
+
+    verifiqueContinuacao(palpite);
+    classifiquePalpite(aleatorio, palpite);
+  }
+
+  cout << "Parabéns, você descobriu o número em " << tentativas << " rodadas"
+       << endl;
+  return 0;
+}
