fc33c2c599cf006d513b09a80a1c542860f5f68d
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-12 18:43:45 -0300

    Adiciona exercício 5.4

---
+++ lista5/4jurosCompostos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+double arredonda(double valor) {
+  int a = (int)(valor * 100);
+  return (double)(a) / 100;
+}
+
+int main() {
+#define ANOS 10
+#define TAXA 0.05
+
+  double entrada;
+  cout << "Inicio : ";
+  cin >> entrada;
+
+  for (short i = 1; i <= ANOS; i++) {
+    entrada += entrada * TAXA;
+    cout << "A soma é " << arredonda(entrada) << " Reais após o ano " << i
+         << endl;
+  }
+
+  cout << "Ao fim : " << arredonda(entrada) << " Reais" << endl;
+
+  return 0;
+}
