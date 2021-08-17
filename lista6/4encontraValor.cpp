e65985cadce45142ba6a4b9ef7c5324ec589b003
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-17 00:38:02 -0300

    Adiciona exercício 6.4

---
+++ lista6/4encontraValor.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+typedef unsigned short ushort;
+
+#define TAMANHO 20
+
+void leiaVetor(int vetor[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    cout << "Digite um valor inteiro: ";
+    cin >> vetor[i];
+  }
+}
+
+ushort encontreValor(const int vetor[], int valor) {
+  short posicao = -1;
+
+  for (short i = 0; i < TAMANHO; i++) {
+    if (vetor[i] == valor) {
+      posicao = i;
+    }
+  }
+
+  return posicao;
+}
+
+int main() {
+  int vetor[TAMANHO], entrada;
+  short posicao;
+
+  leiaVetor(vetor);
+
+  cout << "Digite o valor para ser buscado no vetor: ";
+  cin >> entrada;
+
+  posicao = encontreValor(vetor, entrada);
+  if (posicao < 0) {
+    cout << "Valor não encontrado no vetor";
+  } else {
+    cout << "Valor encontrado na posição " << ++posicao;
+  }
+
+  cout << endl;
+  return 0;
+}
