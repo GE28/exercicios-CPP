53c563e06972d0d7e4e21fd48908b48f8d8c8e2f
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-16 23:43:55 -0300

    Adiciona exercício 6.1

---
+++ lista6/1vetor12posicoes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+typedef unsigned short ushort;
+
+#define TAMANHO 12
+
+ushort obtenhaPosicao() {
+  ushort p;
+  cout << "Informe um número (de 1 a 12): ";
+  cin >> p;
+
+  if (p > TAMANHO) {
+    p = p % TAMANHO + 1;
+  }
+
+  return p;
+}
+
+int leiaVetor(int vetor[], int tam) {
+  for (int i = 0; i < tam; i++) {
+    cout << "Informe um valor real: ";
+    cin >> vetor[i];
+  }
+}
+
+int main() {
+  int vetor[TAMANHO], entrada1, entrada2;
+  leiaVetor(vetor, TAMANHO);
+
+  entrada1 = obtenhaPosicao();
+  entrada2 = obtenhaPosicao();
+
+  cout << "A soma do " << entrada1-- << "º e " << entrada2--
+       << "º valores inseridos é " << vetor[entrada1] + vetor[entrada2] << endl;
+  return 0;
+}
