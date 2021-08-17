f0537734c39f38952d8c5b2bc5fdabf3e7b56256
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-17 19:59:27 -0300

    Adiciona exercício 6.9

---
+++ lista6/9fraseJunta.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void retireEspacos(char vetor[], int tamanho) {
+  for (int i = 1; i < tamanho; i++) {
+    if (vetor[i] == ' ') {
+      for (int j = i; j < tamanho - 1; j++) {
+        vetor[j] = vetor[j + 1];
+      }
+    }
+  }
+}
+
+void imprimaFrase(char vetor[], int tamanho) {
+  for (int i = 0; i < tamanho; i++) {
+    cout << vetor[i];
+  }
+
+  cout << endl;
+}
+
+int main() {
+  char vetor[] = "BCC CEFET-RJ 2021.1!";
+  int tamanho = *(&vetor + 1) - vetor;
+
+  retireEspacos(vetor, tamanho);
+  imprimaFrase(vetor, tamanho);
+  return 0;
+}
