ef51f6f086d739ebe30d226d8c37302420791401
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-17 17:19:43 -0300

    Adiciona exercício 6.6

---
+++ lista6/6zerarNegativos.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+typedef unsigned short ushort;
+
+#define TAMANHO 5
+
+void leiaVetor(int vetor[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    cout << "Digite um valor inteiro: ";
+    cin >> vetor[i];
+  }
+}
+
+void zereNegativos(int vetor[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    if (vetor[i] < 0) {
+      vetor[i] = 0;
+    }
+  }
+}
+
+void imprimaVetor(int vetor[]) {
+  cout << '[' << vetor[0];
+  for (int i = 1; i < TAMANHO - 1; i++) {
+    cout << ", " << vetor[i];
+  }
+  cout << ", " << vetor[TAMANHO - 1] << "]" << endl;
+}
+
+int main() {
+  int vetor[TAMANHO], entrada;
+  short posicao;
+
+  leiaVetor(vetor);
+  zereNegativos(vetor);
+  imprimaVetor(vetor);
+  return 0;
+}
