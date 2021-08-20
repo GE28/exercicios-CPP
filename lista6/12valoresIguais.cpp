2ee6be96222683682bd48753c9de1e06e525bee2
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-20 19:45:43 -0300

    Adiciona exercício 6.12

---
+++ lista6/12valoresIguais.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define TAMANHO 10
+
+void leiaVetor(int vetor[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    cout << "Informe um valor inteiro: ";
+    cin >> vetor[i];
+  }
+}
+
+void encontreRepetidos(int valores[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    for (int j = i + 1; j < TAMANHO; j++) {
+      if (valores[i] == valores[j]) {
+        cout << "Valor repetido encontrado (" << valores[i] << ") nas posições "
+             << i + 1 << " e " << j + 1 << endl;
+      }
+    }
+  }
+}
+
+int main() {
+  int vetor[TAMANHO];
+
+  leiaVetor(vetor);
+  encontreRepetidos(vetor);
+
+  return 0;
+}
