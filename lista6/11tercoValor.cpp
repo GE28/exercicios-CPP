9138eb46eb443e0f21f8a28dedfadc04f55e99c3
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-20 17:14:49 -0300

    Adiciona exercício 6.11

---
+++ lista6/11tercoValor.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define TAMANHO 9
+
+void leiaVetor(double vetor[], int rotulo) {
+  for (int i = 0; i < TAMANHO; i++) {
+    cout << "(VETOR " << rotulo
+         << ") "
+            "Digite um valor real: ";
+    cin >> vetor[i];
+  }
+}
+
+void dividaVetor(double vetor[]) {
+  for (int i = 0; i < TAMANHO; i++) {
+    vetor[i] /= 3;
+  }
+}
+
+void imprimaVetor(double vetor[]) {
+  cout << '[' << vetor[0];
+  for (int i = 1; i < TAMANHO - 1; i++) {
+    cout << ", " << vetor[i];
+  }
+  cout << ", " << vetor[TAMANHO - 1] << "]" << endl;
+}
+
+int main() {
+  double vetor1[TAMANHO], vetor2[TAMANHO], vetor3[TAMANHO];
+
+  leiaVetor(vetor1, 1);
+  leiaVetor(vetor2, 2);
+  leiaVetor(vetor3, 3);
+
+  dividaVetor(vetor1);
+  dividaVetor(vetor2);
+  dividaVetor(vetor3);
+
+  cout << "Vetor 1: ";
+  imprimaVetor(vetor1);
+
+  cout << "Vetor 2: ";
+  imprimaVetor(vetor2);
+
+  cout << "Vetor 3: ";
+  imprimaVetor(vetor3);
+
+  return 0;
+}
