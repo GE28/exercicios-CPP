635180f5aecd24c6ae949b40d6bd2b25df533d0c
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-21 15:52:15 -0300

    Melhora algoritmo de ordenação

--- lista6/9fraseJunta.cpp
+++ lista6/9fraseJunta.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 void retireEspacos(char vetor[], int tamanho) {
-  for (int i = 1; i < tamanho; i++) {
-    if (vetor[i] == ' ') {
-      for (int j = i; j < tamanho - 1; j++) {
+  short casasVazias = 0;
+  int j = 0;
+  for (int i = 0; i < tamanho - casasVazias; i++) {
+    while (vetor[i] == ' ') {
+      for (j = i; j < tamanho - 1; j++) {
+        int jOriginal = vetor[j];
         vetor[j] = vetor[j + 1];
+        vetor[j + 1] = jOriginal;
       }
+
+      casasVazias += 1;
+      i -= 1;
     }
   }
 }
@@ -20,7 +27,7 @@ void imprimaFrase(char vetor[], int tamanho) {
 }
 
 int main() {
-  char vetor[] = "BCC CEFET-RJ 2021.1!";
+  char vetor[] = "    BCC      ||CEFET-RJ  ||2021.1!";
   int tamanho = *(&vetor + 1) - vetor;
 
   retireEspacos(vetor, tamanho);
