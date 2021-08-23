71e3294bb823cbaf6ec932fe64bdea19d5c1eb5b
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-22 21:37:41 -0300

    Remove declaração de função desnecessária

--- lista7/5maiores10.cpp
+++ lista7/5maiores10.cpp
@@ -17,21 +17,6 @@ void conteMaiores(int matriz[][TAMANHO]) {
        << endl;
 }
 
-void imprimaMatriz(int matriz[][TAMANHO]) {
-  int j = 0;
-
-  cout << "[" << endl;
-  for (int i = 0; i < TAMANHO; i++) {
-    j = 0;
-    cout << "[" << matriz[i][j] << ", ";
-    for (j = 1; j < TAMANHO - 1; j++) {
-      cout << matriz[i][j] << ", ";
-    }
-    cout << matriz[i][j] << "]," << endl;
-  }
-  cout << "]" << endl;
-}
-
 int main() {
   int matriz[][TAMANHO] = {{12, 13, 9, 8, 15, 2}, {15, 5, 7, 13, 5, 3},
                            {8, 6, 15, 2, 4, 10},  {19, 12, 7, 2, 8, 9},
