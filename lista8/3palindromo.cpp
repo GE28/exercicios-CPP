1a902ce6720874619e9ccb1d3f019c20df3e8f22
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-24 01:13:56 -0300

    Adiciona exercício 8.3

---
+++ lista8/3palindromo.cpp
@@ -0,0 +1,39 @@
+// Por conta do enunciado impreciso, este código pode não estar cumprindo
+// com exatidão o propósito desejado
+
+#include <iostream>
+using namespace std;
+
+#define TAMANHO 5
+
+void verifiquePalindromo(char letras[][TAMANHO]) {
+  bool palindromo = true;
+  for (int i = 0; i < TAMANHO; i++) {
+    for (int j = i; j < TAMANHO; j++) {
+      if (letras[i][j] != letras[j][i]) {
+        palindromo = false;
+      }
+    }
+  }
+
+  if (palindromo) {
+    cout << "É um palíndromo";
+  } else {
+    cout << "Não é um palíndromo";
+  }
+
+  cout << endl;
+}
+
+int main() {
+  char letras[][TAMANHO] = {
+      {'S', 'A', 'T', 'O', 'R'}, 
+      {'A', 'R', 'E', 'P', 'O'},
+      {'T', 'E', 'N', 'E', 'T'}, 
+      {'O', 'P', 'E', 'R', 'A'},
+      {'R', 'O', 'T', 'A', 'S'},
+  };
+
+  verifiquePalindromo(letras);
+  return 0;
+}
