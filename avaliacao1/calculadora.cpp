5d5f2d1cde1d9cf4b7955a830051a6b2cdb7393f
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-31 20:02:39 -0300

    Adiciona questão 1

---
+++ avaliacao1/calculadora.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void realizarOperacao(double numero1, double numero2, char tipo) {
+  switch (tipo) {
+  case 'S':
+    cout << "A soma de " << numero1 << " e " << numero2 << " eh "
+         << numero1 + numero2;
+    break;
+  case 'P':
+    cout << "O produto entre " << numero1 << " e " << numero2 << " eh "
+         << numero1 * numero2;
+    break;
+  case 'U':
+    cout << "A subtração de " << numero1 << " por " << numero2 << " eh "
+         << numero1 - numero2;
+    break;
+  case 'D':
+    cout << "A divisão entre " << numero1 << " e " << numero2 << " eh "
+         << numero1 / numero2;
+    break;
+  default:
+    cout << "Operacao invalida! Por favor, execute o programa novamente!";
+    break;
+  }
+
+  cout << endl;
+}
+
+double obterValor() {
+  double d;
+  cin >> d;
+  return d;
+}
+
+int main() {
+  double n1, n2;
+  char tipoOperacao;
+
+  cout << "Escolha uma das seguintes opcoes: " << endl;
+  cout << "S - Soma" << endl;
+  cout << "P - Produto" << endl;
+  cout << "U - Subtração" << endl;
+  cout << "D - Divisão" << endl;
+  cin >> tipoOperacao;
+
+  cout << "Digite dois números: " << endl;
+  n1 = obterValor();
+  n2 = obterValor();
+
+  realizarOperacao(n1, n2, tipoOperacao);
+  return 0;
+}
